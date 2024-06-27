8fb1ddb95413ad32230623cc60a597131b8484b9
pholia <160144579+pholia42@users.noreply.github.com>
2024-06-27 15:19:44 +0800

    ？

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,50 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Login from '../views/Login.vue'
-import FeedbackList from '../views/FeedbackList.vue'
-import FeedbackInfo from '../views/FeedbackInfo.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import { ElMessage } from 'element-plus';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Login
-  }, {
+    component: () => import('../views/Login.vue')
+  },
+  {
     path: '/login',
     name: 'Login',
-    component: Login
-  }, {
+    component: () => import('../views/Login.vue')
+  },
+  {
     path: '/feedbackList',
     name: 'FeedbackList',
-    component: FeedbackList
-  }, {
+    component: () => import('../views/FeedbackList.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
     path: '/feedbackInfo/:taskId',
     name: 'FeedbackInfo',
-    component: FeedbackInfo,
-    props: true
+    component: () => import('../views/FeedbackInfo.vue'),
+    props: true,
+    meta: { requiresAuth: true }
   }
-]
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
-})
+});
 
-export default router
+router.beforeEach(async (to, from, next) => {
+  const token = localStorage.getItem('token');
+
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!token && to.name !== 'Login') {
+      ElMessage.warning('登录已过期，请重新登录');
+      next({ name: 'Login' });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
